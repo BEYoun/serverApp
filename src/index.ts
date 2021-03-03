060450ca5bbfbfc5df6e8e72180d879e9560b6f4
younes beaila <younes.beaila@capgemini.com>
2021-03-03 20:27:40 +0100

    add du .env + cors in server

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { createConnection } from "typeorm";
 import express from "express";
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
+import cors from 'cors';
+
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 
@@ -10,7 +12,12 @@ const main = async () => {
     const conn = await createConnection();
     const app = express();
 
-
+    app.use(
+        cors({
+            origin: process.env.CORS_ORIGIN,
+            credentials: true,
+        })
+    );
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver],
@@ -21,10 +28,13 @@ const main = async () => {
             res
         })
     })
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({
+        app,
+        cors: { origin: "http://localhost:3000" }
+    });
 
     app.listen(5000, () => {
-        console.log("server started on locaddddlhost:5000");
+        console.log("server started on LOCALHOST:5000");
     });
 }
 
